c6d1e5baa28db66f7f89cab4c30b39c700adbdfe
William Whatley <william@catatek.com>
2019-10-09 22:19:56 -0400

    ...

--- src/shared-components/Footer.jsx
+++ src/shared-components/Footer.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { Text, Column, Row } from "../theme";
 import Logo from "../assets/logo_white.svg";
 import { DiAndroid, DiApple } from "react-icons/di";
+import { Helmet } from "react-helmet";
+import { FaInstagram, FaFacebook } from "react-icons/fa";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -13,6 +15,7 @@ const Wrapper = styled.div`
   background: ${props => (props.top ? "#8bc53c" : "#3AAA35")};
   @media (max-width: 500px) {
     display: ${props => (props.top ? "block" : "none")};
+    height: ${props => (props.top ? "250px" : "")};
   }
 `;
 
@@ -48,34 +51,101 @@ const StyledRow = styled(Row)`
     width: 75%;
   }
   @media (max-width: 500px) {
-    width: 90%;
+    width: 85%;
+  }
+`;
+
+const StyledIconRow = styled(Row)`
+  margin-top: 2em;
+  @media (max-width: 780px) {
+    margin-top: 1em;
+  }
+  @media (max-width: 500px) {
+    justify-content: center;
   }
 `;
 
 const StyledLogo = styled.img`
-  width: 275px;
+  width: 250px;
 `;
 
 export class Footer extends Component {
   render() {
     return (
       <div>
+        <Helmet>
+          <script>
+            {`
+      (function() {
+      
+        var DRIFT_CHAT_SELECTOR = ".drift-open-chat";
+        
+        function ready(fn) {
+          if (document.readyState != "loading") {
+            fn();
+          } else if (document.addEventListener) {
+            document.addEventListener("DOMContentLoaded", fn);
+          } else {
+            document.attachEvent("onreadystatechange", function() {
+              if (document.readyState != "loading") fn();
+            });
+          }
+        }
+        
+        function forEachElement(selector, fn) {
+          var elements = document.querySelectorAll(selector);
+          for (var i = 0; i < elements.length; i++) fn(elements[i], i);
+        }
+        function openSidebar(driftApi, event) {
+          event.preventDefault();
+          driftApi.sidebar.open();
+          return false;
+        }
+        ready(function() {
+          drift.on("ready", function(api) {
+            var handleClick = openSidebar.bind(this, api);
+            forEachElement(DRIFT_CHAT_SELECTOR, function(el) {
+              el.addEventListener("click", handleClick);
+            });
+          });
+        });
+      })();
+    `}
+          </script>
+        </Helmet>
         <Wrapper top>
           <Div>
-            <StyledLogo src={Logo} />
+            <div>
+              <StyledLogo src={Logo} />
+              <StyledIconRow>
+                <a href="https://www.facebook.com/urbancrop.us/">
+                  <FaFacebook
+                    color="#fff"
+                    size="1.25em"
+                    style={{ marginRight: ".5em" }}
+                  />
+                </a>
+                <a href="https://www.instagram.com/urbancropus">
+                  <FaInstagram color="#fff" size="1.25em" />
+                </a>
+              </StyledIconRow>
+            </div>
             <StyledRow>
               <StyledColumn>
                 <Text white bold>
                   UrbanCrop
                 </Text>
-                <Text white>Blog</Text>
-                <Text white>Team</Text>
+                <a href="https://urbancrop.io/privacy">
+                  <Text white>Privacy policy</Text>
+                </a>
               </StyledColumn>
               <StyledColumn>
                 <Text white bold>
                   Need Help?
                 </Text>
-                <Text white>Contact us</Text>
+                <a style={{ cursor: "pointer" }} className="drift-open-chat">
+                  <Text white>Contact us</Text>
+                </a>
               </StyledColumn>
               <StyledColumn>
                 <Text white bold>
